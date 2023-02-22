1c6cffa76fec11d029533dcb16db1c20949e9402
kei nishikawa <>
2023-02-22 15:14:47 +0900

    ファイル戻し

---
+++ tanigaki-corporate/components/contents/top/c-topNewsSet01/c-topNewsSet01.vue
@@ -0,0 +1,303 @@
+<template lang="pug">
+	
+	section.c-topNewsSet01
+		.componentWrapper(v-if = "news")
+			.block.is-current
+				.area.is-date(v-bind:class="btnSet")
+					p.date
+						//- span {{ news[0].publishedAtJp }}
+				.area.is-text
+					nuxt-link(:to="'/news/' + news[0].id")
+						p.text
+							span {{ news[0].title }}
+				.area.is-btn(v-bind:class="btnSet" @click="btn = !btn")
+					p.btn
+						span
+						span
+						span
+			.block.is-before(v-bind:class="btnSet")
+				dl
+					.area.is-news(v-for="(list, index) of news" v-if = "index != 0")
+						nuxt-link(:to="'/news/' + list.id")
+							dt.date
+								//- span {{list.publishedAtJp}}
+							dd.text
+								span {{list.title}}
+						
+				.area.is-more
+					nuxt-link(:to="'/news/'")
+						p.text MORE
+
+</template>
+
+<script>
+	import dayjs from 'dayjs';
+	import utc from 'dayjs/plugin/utc';
+	import timezone from 'dayjs/plugin/timezone';
+	export default {
+		name: 'c-topNewsSet01',
+		data () {
+			return {
+				btn: false
+			}
+		},
+		props: {
+			news: {
+				type: Array,
+				required: false,
+			},
+		},
+		beforeMount() {
+			dayjs.extend(utc);
+			dayjs.extend(timezone);
+
+			var publishedAtJp = {
+				'publishedAtJp': ''
+			};
+
+			this.news.forEach(function(element, index){
+				publishedAtJp['publishedAtJp'] = dayjs.utc(element['publishedAt']).tz('Asia/Tokyo').format('YYYY-MM-DD');
+				publishedAtJp['publishedAtJp'] = publishedAtJp['publishedAtJp'].replace(/-/g, '.');
+				Object.assign(element, publishedAtJp);
+			})
+		},
+		mounted() {
+		},
+		methods: {
+		},
+		computed:{
+			btnSet: function(){
+				return {
+					open: this.btn,
+					close: !this.btn
+				}
+			}
+		}
+	}
+</script>
+<style lang="stylus">
+	@import "~/assets/stylus/_s_config"
+	@import "~/assets/stylus/_s_mixin"
+
+	.c-topNewsSet01
+		display flex
+		justify-content flex-end
+		position relative
+		z-index 1000
+		
+		width 100%
+		height 0
+
+		transform translate3d(101%, 0, 0)
+
+		.is-inview &
+			animation-name slideRightLeftIn
+			animation-timing-function cubic-bezier(0.8, 0, 0.170, 1)
+			animation-fill-mode forwards
+			animation-duration 0.5s
+			animation-delay 0.4s
+
+		&>.componentWrapper
+			
+			width 50%
+			max-width 720px
+			min-width 500px
+			
+			height 12svw
+			min-height 100px
+			max-height 150px
+			
+			box-shadow 0 4px 20px rgba(#000, 0.1)
+			transform translate3d(0, -50%, 0)
+
+			.page-leave-active &
+				transform translate3d(101%, -50%, 0)
+				transition all 0.3s 0.3s ease-out
+
+			+MQ_MAX(SP_RES_WID01)
+				width 95%
+				height 8svw
+				min-width 0
+				min-height 80px
+				max-height 130px
+				transform translate3d(0, -80%, 0)
+
+			.block
+				
+				&.is-current
+					display flex
+					flex-wrap wrap
+					width 100%
+					height 100%
+				
+				&.is-before
+					overflow hidden
+					will-change max-height
+					transition max-height 0.8s ease-in-out
+					
+					&.close
+						max-height 0
+					
+					&.open
+						max-height 500px
+
+					.area.is-news
+						a
+							display flex
+
+						.date
+							display flex
+							align-items center
+							width 20%
+							height 100%
+							padding 2% 0
+							color #FFF
+							background-color #E02400
+							justify-content center
+							font-weight bold
+							font-size clamp(1.1rem, 1.5cqw, 1.3rem)
+							
+							+MQ_MAX(SP_RES_WID01)
+								width 94px
+								height 100%
+								font-size 1.2rem
+
+						.text
+							display flex
+							align-items center
+							width 80%
+							padding 2% 2%
+							background-color #FFF
+							box-sizing border-box
+							font-size clamp(1.1rem, 1.5cqw, 1.3rem)
+
+							+MQ_MAX(SP_RES_WID01)
+								width calc(100% - 94px)
+								font-size 1.2rem
+
+							span
+								display block
+								text-overflow ellipsis
+								overflow hidden
+								white-space nowrap
+
+
+				.area
+					container-type inline-size					
+
+					&.is-date
+						display flex
+						align-items center
+						width 20%
+						color #FFF
+						background-color #E02400
+						justify-content center
+						border-top-left-radius 10px
+						border-bottom-left-radius 10px
+						font-weight bold
+						font-size clamp(1.1rem, 2cqw, 1.4rem)
+						transition all 0.3s 0.3s ease
+
+						&.open
+							border-bottom-left-radius 0
+							transition all 0.01s ease
+						
+						+MQ_MAX(SP_RES_WID01)
+							width 94px
+							font-size 1.2rem
+
+					&.is-text
+						display flex
+						align-items center
+						width 70%
+						padding 0 4%
+						background-color #FFF
+						box-sizing border-box
+						font-size clamp(1.1rem, 2cqw, 1.4rem)
+
+						+MQ_MAX(SP_RES_WID01)
+							width calc(100% - 134px)
+
+					&.is-btn
+						cursor pointer
+						display flex
+						align-items center
+						justify-content center
+						width 10%
+						background-color #000
+						transition all 0.3s ease-in-out
+
+						+MQ_MAX(SP_RES_WID01)
+							width 40px
+
+						&:hover
+							background-color #e02400
+
+						&.open
+							.btn
+								span
+									&:nth-of-type(1)
+										top 0
+										width 128%
+										transform translate3d(-38%, 0, 0) rotate(45deg) 
+
+									&:nth-of-type(2)
+										left 0
+										transform translate3d(-100%, 0, 0)
+
+									&:nth-of-type(3)
+										bottom 0
+										width 128%
+										transform translate3d(-38%, 0, 0) rotate(-45deg) 
+
+						.btn
+							overflow hidden
+							position relative
+							width 40%
+							padding-top 40%
+
+							span
+								display block
+								position absolute
+								left 50%
+								width 100%
+								height 2px
+								background-color #FFF
+								border-radius 100px
+								transform translate3d(-50%, 0, 0)
+								transition all 0.3s ease-in-out
+
+								&:nth-of-type(1)
+									top 0
+									transform-origin left
+								&:nth-of-type(2)
+									top 50%
+									margin-top -1px
+								&:nth-of-type(3)
+									bottom 0
+									transform-origin left bottom
+					
+					&.is-news
+						width 100%
+					
+					&.is-more
+						color #FFF
+						font-family 'Oswald', sans-serif
+						font-size clamp(1.0rem, 16cqw, 1.8rem)
+						font-weight bold
+						text-align center
+
+						a
+							display block
+							padding 3%
+							background-color #000
+							border-bottom-left-radius 10px
+							transition all 0.3s ease-in-out
+							&:hover
+								background-color #e02400
+					
+					a
+						color inherit
+						text-decoration none
+
+</style>
---
+++ tanigaki-corporate/nuxt.config.js
@@ -0,0 +1,119 @@
+const containerQueryPolyfill = `if (!("container" in document.documentElement.style)) {import("https://unpkg.com/container-query-polyfill@^0.2.0");}`
+
+
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  privateRuntimeConfig: {
+    apiKey: process.env.API_KEY,
+    serviceId: process.env.SERVICE_DOMAIN,
+  },
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'tanigaki-corporate',
+    htmlAttrs: {
+      lang: 'ja'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+      {
+        type: 'module',
+        hid: 'containerQueryPolyfill',
+        innerHTML: containerQueryPolyfill
+      },
+      // { src: 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.10.4/gsap.min.js' },
+      { src: 'https://pixijs.download/release/pixi.js' },
+      // { src: 'https://sdk.form.run/js/v2/embed.js%22%3E' }
+      
+    ],
+    __dangerouslyDisableSanitizersByTagID: {
+      'containerQueryPolyfill': ['innerHTML'],
+    },
+    
+  },
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    { src: '~/assets/stylus/style.styl' },
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    { src: '~/plugins/globalSet', mode: 'client' },
+    { src: '~/plugins/spanWrap', mode: 'client' },
+    { src: '~/plugins/VueModalTor', mode: 'client' },
+    // { src: '~/plugins/pixiPlugins', ssr: false },
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  // components: true,
+  components: [
+    {
+      path: '@/components/',
+      pathPrefix: false
+    }
+  ],
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules:
+  [
+    "@nuxtjs/axios",
+    'nuxt-microcms-module',
+    '@aceforth/nuxt-optimized-images',
+  ],
+  optimizedImages: {
+    optimizeImages: true,
+    optimizeImagesInDev: true,
+  },
+  microcms: {
+    options: {
+      serviceDomain: process.env.SERVICE_DOMAIN,
+      apiKey: process.env.API_KEY,
+    },
+    mode: process.env.NODE_ENV === 'production' ? 'server' : 'all',
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+      // vendor: ['pixi.js']
+  },
+  loading: '~/components/common/c-loadigSet01/c-loadigSet01.vue',
+  // router: {
+  //   scrollBehavior: function (to, from, savedPosition) {
+  //     return { x: 0, y: 0 }
+  //   }
+  // },
+  router: {
+    extendRoutes(routes, resolve) {
+      routes.push({
+        path: '/achievements/:p',
+        component: resolve(__dirname, 'pages/achievements/index.vue'),
+        name: 'achievementsPage',
+      }),
+      routes.push({
+        path: '/news/:p',
+        component: resolve(__dirname, 'pages/news/index.vue'),
+        name: 'newsPage',
+      })
+    },
+  },
+  gtm: {
+    id: process.env.GTM_ID,
+    pageTracking: true
+  },
+}
