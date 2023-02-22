73b11f8cb01205c37d007921f61ca0f398d02144
kei nishikawa <>
2023-02-22 15:35:41 +0900

    ファイル全削除

--- tanigaki-corporate/plugins/globalSet.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import globalSet from '~/assets/javascript/_j_globalSet/_j_globalSet.js';
-
-Vue.use(globalSet)
--- tanigaki-corporate/plugins/spanWrap.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import spanWrap from '~/assets/javascript/_j_spanWrap/_j_spanWrap.js';
-
-Vue.use(spanWrap)
