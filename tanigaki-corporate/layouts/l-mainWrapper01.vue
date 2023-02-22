7dd6c72e18b59f5e7b2113528b309cdc7a45d979
kei nishikawa <>
2023-02-22 16:03:39 +0900

    ファイル戻し

---
+++ tanigaki-corporate/layouts/l-mainWrapper01.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+	#l-contentsTop
+		.l-mainWrapper01
+			.layoutWrapper
+				nuxt
+
+
+</template>
+<script>
+	export default {
+		
+	};
+</script>
+<style lang="stylus">
+	#l-contentsTop
+		overflow hidden
+
+	
+
+</style>
