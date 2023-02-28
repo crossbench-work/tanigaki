7ad50c14479b3e8d646ae567286d66a624a83be0
syo24 <>
2023-02-28 12:54:47 +0900

    index.html ルートへリダイレクト

--- tanigaki-corporate/nuxt.config.js
+++ tanigaki-corporate/nuxt.config.js
@@ -111,6 +111,10 @@ export default {
         name: 'achievementsPage',
       }),
       routes.push({
+        path: '/index.html',
+        redirect: '/',
+      }),
+      routes.push({
         path: '/news/:p',
         component: resolve(__dirname, 'pages/news/index.vue'),
         name: 'newsPage',
